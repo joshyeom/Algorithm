7fe6b2c8fed8d0e0b10335e1069c05caa0b4998f
염정호(Josh) <105287510+joshyeom@users.noreply.github.com>
2026-02-03 00:46:05 +0900

    Time: 7 ms (72.25%), Space: 58.1 MB (80.14%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findErrorNums = function(nums) {
+    const arr = new Array(nums.length).fill(0);
+
+    for(let i = 0 ; i < nums.length ; i++){
+        arr[nums[i] - 1] += 1
+    }
+
+    console.log(arr);
+
+    return [arr.findIndex((v) => v === 2) + 1, arr.findIndex((v) => v === 0) + 1]
+};
